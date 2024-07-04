64e38aadd9824ba523d4fffa0debd53ef376393e
AliceHancox2007 <alicemittens.10@gmail.com>
2024-07-04 12:57:58 +1200

    New Form (window puzzle)

--- EscapeRoom/Properties/Resources.Designer.cs
+++ EscapeRoom/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace EscapeRoom.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap CloseUpWindow {
+            get {
+                object obj = ResourceManager.GetObject("CloseUpWindow", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap DoorLayout_1 {
             get {
                 object obj = ResourceManager.GetObject("DoorLayout 1", resourceCulture);
